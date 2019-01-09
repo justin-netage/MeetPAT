c6349c974678e728bcccea4bfcb4a4cf57225625
justin-netage <justin@netage.co.za>
2019-01-09 11:04:02 +0200

    Updated to environment variables for facebook sdk.

--- routes/web.php
+++ routes/web.php
@@ -62,3 +62,91 @@ Route::get('/meetpat-client/sync-platform', 'MeetpatClientController@sync_platfo
 Route::get('/meetpat-client/upload-clients', 'MeetpatClientController@upload_clients')->name('meetpat-client-upload')->middleware('auth')->middleware('client');
 
 
+// Facebook Login Routes
+
+Route::get('/fb-callback', function() {
+        $fb = new Facebook\Facebook([
+        'app_id' => env('FACEBOOK_APP_SECRET'),
+        'app_secret' => env('FACEBOOK_APP_ID'),
+        'default_graph_version' => 'v2.10',
+        ]);
+      
+        $helper = $fb->getRedirectLoginHelper();
+        
+        $permissions = ['email']; // Optional permissions
+        $loginUrl = $helper->getLoginUrl('https://infinite-coast-17182.herokuapp.com/fb-callback', $permissions);
+        
+        echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+});
+
+Route::get('/facebook-login', function() {
+    $fb = new Facebook\Facebook([
+        'app_id' => env('FACEBOOK_APP_SECRET'),
+        'app_secret' => env('FACEBOOK_APP_ID'),
+        'default_graph_version' => 'v2.10',
+        ]);
+      
+      $helper = $fb->getRedirectLoginHelper();
+      
+      try {
+        $accessToken = $helper->getAccessToken();
+      } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+      } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+      }
+      
+      if (! isset($accessToken)) {
+        if ($helper->getError()) {
+          header('HTTP/1.0 401 Unauthorized');
+          echo "Error: " . $helper->getError() . "\n";
+          echo "Error Code: " . $helper->getErrorCode() . "\n";
+          echo "Error Reason: " . $helper->getErrorReason() . "\n";
+          echo "Error Description: " . $helper->getErrorDescription() . "\n";
+        } else {
+          header('HTTP/1.0 400 Bad Request');
+          echo 'Bad request';
+        }
+        exit;
+      }
+      
+      // Logged in
+      echo '<h3>Access Token</h3>';
+      var_dump($accessToken->getValue());
+      
+      // The OAuth 2.0 client handler helps us manage access tokens
+      $oAuth2Client = $fb->getOAuth2Client();
+      
+      // Get the access token metadata from /debug_token
+      $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+      echo '<h3>Metadata</h3>';
+      var_dump($tokenMetadata);
+      
+      // Validation (these will throw FacebookSDKException's when they fail)
+      $tokenMetadata->validateAppId($config['app_id']);
+      // If you know the user ID this access token belongs to, you can validate it here
+      //$tokenMetadata->validateUserId('123');
+      $tokenMetadata->validateExpiration();
+      
+      if (! $accessToken->isLongLived()) {
+        // Exchanges a short-lived access token for a long-lived one
+        try {
+          $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+          echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
+          exit;
+        }
+      
+        echo '<h3>Long-lived</h3>';
+        var_dump($accessToken->getValue());
+      }
+      
+      $_SESSION['fb_access_token'] = (string) $accessToken;    
+});
+
+
+
