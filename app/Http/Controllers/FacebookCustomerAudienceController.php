6497a800fcb0e76ff6dabd0e9c1f9b8b3abc0aa3
justin-netage <justin@netage.co.za>
2019-01-10 06:59:12 +0200

    Testing code snipet from marketing api php sdk.

--- app/Http/Controllers/FacebookCustomerAudienceController.php
+++ app/Http/Controllers/FacebookCustomerAudienceController.php
@@ -3,6 +3,9 @@
 namespace MeetPAT\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 
 class FacebookCustomerAudienceController extends Controller
 {
@@ -10,23 +13,38 @@ class FacebookCustomerAudienceController extends Controller
 
     public function register_ad_account_id(Request $request) 
     {
-        $access_token_stored = false;
-
-        if($request->session()->get('facebook_accessToken')) {
-            $access_token_stored = $request->session()->get('facebook_accessToken');
-        }
-
-        $fb = new \Facebook\Facebook([
-            'app_id' => env('FACEBOOK_APP_ID'),
-            'app_secret' => env('FACEBOOK_APP_SECRET'),
-            'default_graph_version' => 'v2.10',
-            ]);
+        $fb = new Facebook([
+            'app_id' => '{your-app-id}',
+            'app_secret' => '{your-app-secret}',
+          ]);
           
+          $helper = $fb->getRedirectLoginHelper();
+          
+          if ($request->session()->exists('facebook_access_token')) {
+            $_SESSION['facebook_access_token'] = null;
+          }
+          
+          if ($request->session()->exists('facebook_access_token')) {
             $helper = $fb->getRedirectLoginHelper();
-            // $accessToken = $fb->getAccessToken();
-            
-            $permissions = ['email']; // Optional permissions
-            $loginUrl = $helper->getLoginUrl('https://infinite-coast-17182.herokuapp.com/register-facebook-add-account', $permissions);
+            try {
+                $request->session()->put('key', (string) $helper->getAccessToken());
+            } catch(FacebookResponseException $e) {
+              // When Graph returns an error
+              echo 'Graph returned an error: ' . $e->getMessage();
+              exit;
+            } catch(FacebookSDKException $e) {
+              // When validation fails or other local issues
+              echo 'Facebook SDK returned an error: ' . $e->getMessage();
+              exit;
+            }
+          }
+          
+          if ($request->session()->exists('facebook_access_token')) {
+            echo "You are logged in!";
+          } else {
+            $permissions = ['ads_management'];
+            $loginUrl = $helper->getLoginUrl('http://localhost:8888/marketing-api/', $permissions);
+          }
 
             return view('auth.facebook_ad_account', ['login_url' => $loginUrl, 'access_token_stored' => $access_token_stored]);
     }
