8ef0b5344ba7449aa4fcdb09d3a52d2c815483fa
justin-netage <justin@netage.co.za>
2019-01-25 09:32:11 +0200

    Removed conditional for testing.

--- app/Http/Controllers/Auth/GoogleLoginController.php
+++ app/Http/Controllers/Auth/GoogleLoginController.php
@@ -27,14 +27,13 @@ class GoogleLoginController extends Controller
     public function handleProviderCallback()
     {
         $user = Socialite::driver('google')->user();
+        //$has_google_account = $user->ad_word_account();
 
-        $has_google_account = $user->ad_word_account();
-
-        if($has_google_account) {
-            $has_google_account->update(['access_token' => $user->token]);
-        } else {
+        // if($has_google_account) {
+        //     $has_google_account->update(['access_token' => $user->token]);
+        // } else {
             \MeetPAT\GoogleAdwordsAccount::create(['access_token' => $user->token, 'user_id' => $user->id, 'ad_account_id' => null]);
-        }
+        // }
     }
 
 }
