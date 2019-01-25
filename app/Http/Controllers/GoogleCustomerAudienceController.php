bd01e6799ef7b2440d93fef6245206decbad6369
justin-netage <justin@netage.co.za>
2019-01-25 10:17:51 +0200

    Testing accesstoken retrieval.

--- app/Http/Controllers/GoogleCustomerAudienceController.php
+++ app/Http/Controllers/GoogleCustomerAudienceController.php
@@ -11,6 +11,16 @@ class GoogleCustomerAudienceController extends Controller
     {
         $user = \Auth::User();
 
+        $user_google_account = Socialite::driver('google')->user();
+
+        if($user_google_account) {
+            \Session::flash('success', 'Your Access token: ' . $user_google_account->access_token);
+        } else {
+            \Session::flash('error', 'Your Google Access Token could not be container');
+
+        }
+
+
         return view('auth.google_ad_account', []);
 
     }
