a12db42d2547f2e7f17b7655b90a5f0af2f9eb72
justin-netage <justin@netage.co.za>
2019-04-16 12:53:31 +0200

    Slight changes on google sync page

--- resources/views/client/dashboard/sync_google_acc.blade.php
+++ resources/views/client/dashboard/sync_google_acc.blade.php
@@ -4,11 +4,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-6 col-offset-3">
-            <a href="{{$auth_uri}}" target="_blank" class="btn btn-danger btn-lg btn-block rounded-0 shadow-block shadow-block">Get Authorization Code</a>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-6 col-offset-3">
             <div class="card">
                 <div class="card-header"><h1 id="card-title">{{ __('Sync Google Account') }} &nbsp;<i class="fab fa-google"></i> </h1></div>
                     <div class="card-body">
@@ -16,6 +11,7 @@
                         <!-- Make route /google/authenticate-code and add controller to save google ads account and access code -->
                         <form id="upload-custom-audience" method="post" action="/google-authorization/authenticate-authorization-code" onsubmit="displayLoader();">
                         @csrf
+                        <input type="hidden" name="user_id" value="{{\Auth::user()->id}}">
                         <div class="form-group">
                             <label for="adwords_id">{{__('Google Ads Account ID') }}</label>
                             <input type="text" name="adwords_id" id="adwords-id" placeholder="123-456-7890" value="{{ old('adwords_id') }}" class="form-control{{ $errors->has('adwords_id') ? ' is-invalid' : '' }}" autofocus>
@@ -25,11 +21,13 @@
                                 </span>
                             @endif
                         </div>
-                        <div class="form-group">
-                            <label for="auth_code">{{ __('Authorization Code') }}</label>
-                            <input type="hidden" name="user_id" value="{{\Auth::user()->id}}">
-                            <input id="auth_code" name="auth_code" type="text" placeholder="Enter your Authorization Code" max="1000" class="form-control{{ $errors->has('auth_code') ? ' is-invalid' : '' }}" name="auth_code" value="{{ old('auth_code') }}">
+                        <div class="input-group mb-3">
+                            <label for="auth_code" class="w-100">{{ __('Authorization Code') }}</label>
 
+                            <input id="auth_code" name="auth_code" type="text" placeholder="Enter your Authorization Code" max="1000" class="form-control{{ $errors->has('auth_code') ? ' is-invalid' : '' }}" name="auth_code" value="{{ old('auth_code') }}">
+                            <div class="input-group-append">
+                                <a href="{{$auth_uri}}" target="_blank" class="btn btn-outline-danger shadow-block" type="button" id="button-addon2">Get Code</a>
+                            </div>
                             @if ($errors->has('auth_code'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('auth_code') }}</strong>
