eee3e2cebac6cc7999e0fb520cf31e0a2229a873
justin-netage <justin@netage.co.za>
2019-06-28 09:04:16 +0200

    Fixed incorrect job call.

--- app/Http/Controllers/DataVisualisationController.php
+++ app/Http/Controllers/DataVisualisationController.php
@@ -73,7 +73,7 @@ class DataVisualisationController extends Controller
         } else {
             return response("file does not exist :(");
         }
-        \MeetPAT\Jobs\ProcessFile::dispatch()->onQueue('Prepare BSA File');
+        \MeetPAT\Jobs\EnrichRecords::dispatch()->onQueue('Prepare BSA File');
         return response()->json($created_job_que);
     }
 
