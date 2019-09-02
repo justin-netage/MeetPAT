e5d3bcac2d11b942b54d480ab4401ffe6a91552b
justin-netage <justin@netage.co.za>
2019-09-02 10:07:41 +0200

    Added favicon.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <link rel="shortcut icon" href="{{ Storage::disk('s3')->url('dashboard.meetpat/public/images/Minimal Logo.png') }}">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
