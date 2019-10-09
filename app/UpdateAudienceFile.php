f326662f827156d88329d7359af9dfa398bc406d
justin-netage <justin@netage.co.za>
2019-10-09 09:53:32 +0200

    Added Missing relation with Update model

--- app/UpdateAudienceFile.php
+++ app/UpdateAudienceFile.php
@@ -8,4 +8,10 @@ class UpdateAudienceFile extends Model
 {
     //
     protected $fillable = ['user_id', 'audience_name', 'file_unique_name'];
+
+    public function job() 
+    {
+
+        return $this->hasOne('\MeetPAT\UpdateRecordsJobQueue', 'audience_file_id', 'id');
+    }
 }
--- app/UpdateRecordsJobQueue.php
+++ app/UpdateRecordsJobQueue.php
@@ -8,4 +8,9 @@ class UpdateRecordsJobQueue extends Model
 {
     //
     protected $fillable = ['user_id', 'audience_file_id', 'status', 'records'];
+
+    public function audience_file()
+    {
+        return $this->belongsTo('\MeetPAT\UpdateAudienceFile', 'audience_file_id');
+    }
 }
