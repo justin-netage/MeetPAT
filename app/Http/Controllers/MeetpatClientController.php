dc05ca718bfd44d99753a2b96c50c54dd17f0dd5
justin-netage <justin@netage.co.za>
2019-10-10 10:29:56 +0200

    Changed from csv to xlsx.

--- app/Http/Controllers/MeetpatClientController.php
+++ app/Http/Controllers/MeetpatClientController.php
@@ -1427,23 +1427,23 @@ class MeetpatClientController extends Controller
         foreach($files_array as $key=>$file)
         {
             if(env('APP_ENV') == 'production') {
-                if(\Storage::disk('s3')->exists('client/saved-audiences/' . 'user_id_' . $request->user_id . '/' . $files_array[$key]["file_name"] . '.csv'))
+                if(\Storage::disk('s3')->exists('client/saved-audiences/' . 'user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.xlsx'))
                 {
-                    $files_array->items()[$key]["download"] = \Storage::disk('s3')->temporaryUrl('client/saved-audiences/user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.csv', now()->addMinutes(5),
+                    $files_array->items()[$key]["download"] = \Storage::disk('s3')->temporaryUrl('client/saved-audiences/user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.xlsx', now()->addMinutes(5),
                      ['Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                       'ResponseContentType' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                      'ResponseContentDisposition' => 'attachment; filename=' . $files_array->items()[$key]["file_name"] . ".csv"]);
-                    $files_array->items()[$key]["size"] = round(\Storage::disk('s3')->size('client/saved-audiences/user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.csv') / 1024 / 1024, 2) . "MB";
+                      'ResponseContentDisposition' => 'attachment; filename=' . $files_array->items()[$key]["file_name"] . ".xlsx"]);
+                    $files_array->items()[$key]["size"] = round(\Storage::disk('s3')->size('client/saved-audiences/user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.xlsx') / 1024 / 1024, 2) . "MB";
                 } else {
                     $files_array->items()[$key]["download"] = "/404";
                     $files_array->items()[$key]["size"] = "N\A";
                 }
     
             } else {
-                if(\Storage::disk('local')->exists('client/saved-audiences/' . 'user_id_' . $request->user_id . '/' . $files_array[$key]["file_name"] . '.csv'))
+                if(\Storage::disk('local')->exists('client/saved-audiences/' . 'user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.xlsx'))
                 {
                     $files_array->items()[$key]["download"] = "/404";
-                    $files_array->items()[$key]["size"] = round(\Storage::disk('local')->size('client/saved-audiences/user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.csv') / 1024 / 1024, 2) . "MB";
+                    $files_array->items()[$key]["size"] = round(\Storage::disk('local')->size('client/saved-audiences/user_id_' . $request->user_id . '/' . $files_array[$key]["file_unique_name"] . '.xlsx') / 1024 / 1024, 2) . "MB";
                 } else {
                     $files_array->items()[$key]["download"] = "/404";
                     $files_array->items()[$key]["size"] = "N\A";
